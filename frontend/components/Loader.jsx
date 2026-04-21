73ff659eea8bfcc847dbf3f90128aca115d9aaf1
Shakya77 <bijanshakya145@gmail.com>
2026-04-21 20:03:29 +0545

    few changes

--- frontend/components/Loader.jsx
+++ frontend/components/Loader.jsx
@@ -1,9 +1,23 @@
 import { Atom } from "react-loading-indicators";
+import { cn } from "@/lib/utils";
 
-export default function Loader() {
+export default function Loader({
+  fullScreen = true,
+  className = "",
+  size = "medium",
+  color = "#474947",
+}) {
+  if (fullScreen) {
+    return (
+      <div className="fixed inset-0 z-50 flex h-screen w-screen items-center justify-center bg-black/50 backdrop-blur-[8px]">
+        <Atom color={color} size={size} text="" textColor="" />
+      </div>
+    );
+  }
+
   return (
-    <div className="fixed inset-0 z-9999 flex h-screen w-screen items-center justify-center bg-black/50 backdrop-blur-[8px]">
-      <Atom color="#474947" size="medium" text="" textColor="" />
+    <div className={cn("flex items-center justify-center", className)}>
+      <Atom color={color} size={size} text="" textColor="" />
     </div>
   );
 }
